d381ab7ac4db83e164cf6ba9f5b66147bd2d7f2d
RedGuy12 <RedGuy12@users.noreply.github.com>
2021-07-05 18:40:16 +0000

    deploy: e6cd5680d00d5d3d313d461a48394abefa86ce80

---
+++ _nuxt/static/1625510384/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fintroduction","\u002Finstallation","\u002Ferror-handlers","\u002Fmodule","\u002Fmethods","\u002Ferrors","\u002Fmodules"]})
